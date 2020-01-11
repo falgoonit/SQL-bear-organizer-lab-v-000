35a27629267c4ace32710b69b6bd7be75a8a16dc
Falgoon Patel <falgoonit@gmail.com>
2020-01-11 23:51:15 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -8,7 +8,7 @@ INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES ('Tabitha', 6, 'F', 'Dark Brown', 'Calm', 1);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ('Sergeant Brown', 12, 'M', 'Brown', 'Irritable', 0);
+VALUES ('Sergeant Brown', 12, 'M', 'Brown', 'Irritable', 1);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES ('Melissa', 13, 'F', 'Blonde', 'Calm', 1);
