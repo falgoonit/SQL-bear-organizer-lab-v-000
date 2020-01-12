0abcf4581e3510645f52db057f1141d1c22af558
Falgoon Patel <falgoonit@gmail.com>
2020-01-12 00:33:30 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,23 +1,23 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ('Mr. Chocolate', 12, 'M', 'Dark Brown', 'Calm', 1);
+VALUES ('Mr. Chocolate', 12, 'M', 'dark brown', 'Calm', 1);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ('Rowdy', 8, 'M', 'Dark Brown', 'Excitable', 1);
+VALUES ('Rowdy', 8, 'M', 'dark brown', 'Excitable', 1);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ('Tabitha', 6, 'F', 'Dark Brown', 'Calm', 1);
+VALUES ('Tabitha', 6, 'F', 'white', 'Calm', 1);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ('Sergeant Brown', 12, 'M', 'Brown', 'Irritable', 1);
+VALUES ('Sergeant Brown', 12, 'M', 'dark brown', 'Irritable', 1);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ('Melissa', 13, 'F', 'Blonde', 'Calm', 1);
+VALUES ('Melissa', 13, 'F', 'white', 'Calm', 1);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES ('Grinch', 22, 'M', 'White', 'Angry', 1);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ('Wendy', 6, 'F', 'Cinnamon', 'Calm', 1);
+VALUES ('Wendy', 6, 'F', 'black', 'Calm', 1);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES (NULL, 20, 'M', 'black', 'aggressive', 0);
